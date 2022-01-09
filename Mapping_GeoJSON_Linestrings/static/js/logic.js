ef58f56cfc900bb6aff716ce8663ff65c840d655
Jessica Howard <jessicahoward@Jessicas-MBP.fios-router.home>
2022-01-08 21:06:30 -0500

    adding GeoJSON linestring

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -29,16 +29,25 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/jhoward19/Mapping_Earthquakes/main/torontoRoutes.json";
+
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
-        onEachFeature: function(feature, layer) {
-            console.log(layer);
-            layer.bindPopup("<h2> Airport code: " + feature.properties.faa + " </h2><h3><p>Airport name: " + feature.properties.name);
-        }
-    }).addTo(map);
+            style: myStyle,
+            onEachFeature: function(feature, layer) {
+                console.log(layer);
+                layer.bindPopup("<h3> Airline:  " + feature.properties.airline + " </h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+            }
+        })
+        .addTo(map);
 });
